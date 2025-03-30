d0c50dc27b0ad4c840d6ab5eb65dcdb678af8c84
Fahmi Hasan <mochammadfahmihasan@gmail.com>
2025-03-31 01:13:45 +0700

    memperbaiki relasi model

--- backend/models/plans.js
+++ backend/models/plans.js
@@ -4,30 +4,31 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class plans extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      plans.belongsTo(models.users, { foreignKey: 'user_id', as: 'users' });
+      plans.belongsTo(models.users, { foreignKey: 'user_id', as: 'user' });
+      plans.hasMany(models.plan_menus, { foreignKey: 'plan_id', as: 'plan_menus', onDelete: 'CASCADE', });
     }
   }
-  plans.init({
-    plan_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
+
+  plans.init(
+    {
+      plan_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_id: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      plan_start: DataTypes.DATE,
+      plan_end: DataTypes.DATE,
+      status: DataTypes.ENUM('draft', 'ongoing', 'completed', 'cancelled'),
     },
-    user_id: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    plan_start: DataTypes.DATE,
-    plan_end: DataTypes.DATE,
-    status: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'plans',
-  });
+    {
+      sequelize,
+      modelName: 'plans',
+    }
+  );
+
   return plans;
 };
--- backend/models/users.js
+++ backend/models/users.js
@@ -2,36 +2,37 @@
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       users.hasMany(models.plans, { foreignKey: 'user_id', as: 'plans' });
     }
   }
-  users.init({
-    user_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
+
+  users.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      age: DataTypes.INTEGER,
+      gender: DataTypes.ENUM('male', 'female'),
+      weight: DataTypes.INTEGER,
+      height: DataTypes.INTEGER,
+      activity_level: DataTypes.ENUM('low', 'medium', 'high'),
+      goal: DataTypes.ENUM('lose_weight', 'maintain_weight', 'gain_weight'),
     },
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    age: DataTypes.INTEGER,
-    gender: DataTypes.ENUM('male', 'female'),
-    weight: DataTypes.INTEGER,
-    height: DataTypes.INTEGER,
-    activity_level: DataTypes.ENUM('low', 'medium', 'high'),
-    goal: DataTypes.ENUM('lose_weight', 'maintain_weight', 'gain_weight')
-  }, {
-    sequelize,
-    modelName: 'users',
-  });
+    {
+      sequelize,
+      modelName: 'users',
+    }
+  );
+
   return users;
 };
